2e525a8013b1def6404370372faba6893411a841
Tyler Neylon <tylerneylon@gmail.com>
2023-05-29 21:18:56 -0700

    Build html loader with cache.

--- get_book_data.py
+++ get_book_data.py
@@ -1,5 +1,35 @@
 #!/usr/bin/env python3
+
+# ______________________________________________________________________
+# Imports
+
+import base64
+from pathlib import Path
 from urllib import request
 
-with request.urlopen('http://www.python.org/') as f:
-    print(f.read())
+
+# ______________________________________________________________________
+# Globals
+
+test_url_1 = 'https://www.goodreads.com/review/list_rss/3534041?shelf=read'
+
+# ______________________________________________________________________
+# Functions
+
+def get_w_cache(url):
+    fname = Path('cache/' + base64.b32encode(url.encode()).decode())
+    if fname.is_file():
+        with fname.open() as f:
+            return f.read()
+    with request.urlopen(url) as f:
+        data = f.read()
+    with fname.open('wb') as f:
+        f.write(data)
+    return data
+
+
+# ______________________________________________________________________
+# Main
+
+data = get_w_cache(test_url_1)
+print(data[:20])
